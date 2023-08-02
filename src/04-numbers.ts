43bbcd428f8ae6345a85107904a523a0cc11be85
Ramses Garay <senkustone15@gmail.com>
2023-08-02 08:02:36 -0600

    numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,24 @@
+(() => {
+let productPrice = 100
+productPrice = 12
+console.log('ProductPrice', productPrice)
+
+
+let customerAge: number = 28
+customerAge += 1
+console.log('customerAge',customerAge)
+
+// si dejamos el valor de la variable vacio
+//ts nos avisara que seguramente sera undefined.
+let productInStock: number = 1;
+console.log('productInStock', productInStock)
+
+let discount = parseInt('123')
+console.log('discount', discount)
+
+let hex = 0xfff
+console.log(hex)
+let bin = 0b0100110101
+console.log(bin)
+})()
+
